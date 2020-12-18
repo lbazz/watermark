d67ceed6d2166323ab7cdba6d6ff34e1d67e1ec8
LBAZZ <luiz.dsg@gmail.com>
2020-12-18 00:20:45 -0300

    Adição de posicionamento de imagem via coordenadas.
    Adição de fonte custom.

--- scripts/watermark.bundle.js
+++ scripts/watermark.bundle.js
@@ -905,14 +905,21 @@ watermark.destroy = function () {
 
 var watermark = require('../node_modules/watermarkjs');
 
-var author        = document.querySelector("#author"),
-    authorColor   = document.querySelector("#author-color"),
-    title         = document.querySelector("#title"),
-    titleColor    = document.querySelector("#title-color"),
-    uCoverImage   = document.querySelector("#cover-image"),
-    uWatermark    = document.querySelector("#watermark"),
-    processImage  = document.querySelector("#proccess-image"),
-    downloadImage = document.querySelector("#download-image");
+var author          = document.querySelector("#author"),
+    authorColor     = document.querySelector("#author-color"),
+    authorPosX      = document.querySelector("#author-position-x"),
+    authorPosY      = document.querySelector("#author-position-y"),
+    title           = document.querySelector("#title"),
+    titleColor      = document.querySelector("#title-color"),
+    titlePosX       = document.querySelector("#title-position-x"),
+    titlePosY       = document.querySelector("#title-position-y"),
+    uCoverImage     = document.querySelector("#cover-image"),
+    uWatermark      = document.querySelector("#watermark"),
+    uWatermarkOp    = document.querySelector("#watermark-opacity"),
+    uWatermarkPosX  = document.querySelector("#watermark-position-x"),
+    uWatermarkPosY  = document.querySelector("#watermark-position-y"),
+    processImage    = document.querySelector("#proccess-image"),
+    downloadImage   = document.querySelector("#download-image");
     
 // Process the image.
 processImage.addEventListener("click", function(e) {
@@ -924,19 +931,20 @@ processImage.addEventListener("click", function(e) {
     applyWatermark({
         'author': author.value,
         'authorColor': authorColor.value,
+        'authorPosX': authorPosX.value,
+        'authorPosY': authorPosY.value,
         'title': title.value,
         'titleColor': titleColor.value,
+        'titlePosX': titlePosX.value,
+        'titlePosY': titlePosY.value,
         'coverImage': uCoverImage.files[0],
         'watermarkImage': uWatermark.files[0],
+        'watermarkOpacity': uWatermarkOp.value,
+        'watermarkPosX': uWatermarkPosX.value,
+        'watermarkPosY': uWatermarkPosY.value,
     });
 
 }, false);
-
-// Download the image.
-downloadImage.addEventListener("click", function(e) {
-    
-}, false);
-
 
 function applyWatermark(infoToProcess) {
     console.log(infoToProcess);
@@ -947,48 +955,62 @@ function applyWatermark(infoToProcess) {
         }
     };
 
+    var authorPosX = function(image1, image2, context) {
+      return infoToProcess.authorPosX;
+    };
+    
+    var authorPosY = function(image1, image2, context) {
+      return infoToProcess.authorPosY;
+    };
+
+    var titlePosX = function(image1, image2, context) {
+      return infoToProcess.titlePosX;
+    };
+    
+    var titlePosY = function(image1, image2, context) {
+      return infoToProcess.titlePosY;
+    };
+    
+    var watermarkPosX = function(image1, image2) {
+      return infoToProcess.watermarkPosX;
+    };
+    
+    var watermarkPosY = function(image1, image2) {
+      return infoToProcess.watermarkPosY;
+    };
+
+
     watermark([infoToProcess.coverImage, infoToProcess.watermarkImage], options)
-        .image(watermark.image.center(0.5))
+        .image(watermark.image.atPos(
+            watermarkPosX,
+            watermarkPosY,
+            infoToProcess.watermarkOpacity
+        ))
         .render()
-        .image(watermark.text.lowerLeft(
+        .image(watermark.text.atPos(
+            authorPosX,
+            authorPosY,
             infoToProcess.author,
-            '16px Josefin Slab',
+            '16px Aladin, cursive',
             infoToProcess.authorColor,
             1
         ))
         .render()
-        .image(watermark.text.lowerRight(
+        .image(watermark.text.atPos(
+            titlePosX,
+            titlePosY,
             infoToProcess.title,
-            '30px Josefin Slab',
+            '30px Aladin, cursive',
             infoToProcess.titleColor,
             1
-        ))     
+        ))  
         .then(function (img) {
             img.id = "new-image";
             downloadImage.style.display = "block";
 
+            document.getElementById('result-image').innerHTML = '';
             document.getElementById('result-image').appendChild(img);
             downloadImage.href = document.getElementById('new-image').src;
-        //console.log(document.getElementById('alpha-image'));
         });
 }
-
-/*
-watermark([uCoverImage, uWatermark], options)
-  .image(watermark.image.lowerRight(0.5))
-  .then(function (img) {
-    document.getElementById('alpha-image').appendChild(img);
-    //console.log(document.getElementById('alpha-image'));
-  });
-  
-*/
-  
-/*
-
-watermark(['http://web.com/a.jpg', 'http://web.com/b.jpg'], options);
-
-// load a url and a file object
-var upload = document.querySelector('input[type=file]').files[0];
-watermark(['/img/photo.jpg', upload]);
-*/
 },{"../node_modules/watermarkjs":1}]},{},[2]);
--- scripts/watermark.js
+++ scripts/watermark.js
@@ -2,14 +2,21 @@
 
 var watermark = require('../node_modules/watermarkjs');
 
-var author        = document.querySelector("#author"),
-    authorColor   = document.querySelector("#author-color"),
-    title         = document.querySelector("#title"),
-    titleColor    = document.querySelector("#title-color"),
-    uCoverImage   = document.querySelector("#cover-image"),
-    uWatermark    = document.querySelector("#watermark"),
-    processImage  = document.querySelector("#proccess-image"),
-    downloadImage = document.querySelector("#download-image");
+var author          = document.querySelector("#author"),
+    authorColor     = document.querySelector("#author-color"),
+    authorPosX      = document.querySelector("#author-position-x"),
+    authorPosY      = document.querySelector("#author-position-y"),
+    title           = document.querySelector("#title"),
+    titleColor      = document.querySelector("#title-color"),
+    titlePosX       = document.querySelector("#title-position-x"),
+    titlePosY       = document.querySelector("#title-position-y"),
+    uCoverImage     = document.querySelector("#cover-image"),
+    uWatermark      = document.querySelector("#watermark"),
+    uWatermarkOp    = document.querySelector("#watermark-opacity"),
+    uWatermarkPosX  = document.querySelector("#watermark-position-x"),
+    uWatermarkPosY  = document.querySelector("#watermark-position-y"),
+    processImage    = document.querySelector("#proccess-image"),
+    downloadImage   = document.querySelector("#download-image");
     
 // Process the image.
 processImage.addEventListener("click", function(e) {
@@ -21,10 +28,17 @@ processImage.addEventListener("click", function(e) {
     applyWatermark({
         'author': author.value,
         'authorColor': authorColor.value,
+        'authorPosX': authorPosX.value,
+        'authorPosY': authorPosY.value,
         'title': title.value,
         'titleColor': titleColor.value,
+        'titlePosX': titlePosX.value,
+        'titlePosY': titlePosY.value,
         'coverImage': uCoverImage.files[0],
         'watermarkImage': uWatermark.files[0],
+        'watermarkOpacity': uWatermarkOp.value,
+        'watermarkPosX': uWatermarkPosX.value,
+        'watermarkPosY': uWatermarkPosY.value,
     });
 
 }, false);
@@ -38,26 +52,60 @@ function applyWatermark(infoToProcess) {
         }
     };
 
+    var authorPosX = function(image1, image2, context) {
+      return infoToProcess.authorPosX;
+    };
+    
+    var authorPosY = function(image1, image2, context) {
+      return infoToProcess.authorPosY;
+    };
+
+    var titlePosX = function(image1, image2, context) {
+      return infoToProcess.titlePosX;
+    };
+    
+    var titlePosY = function(image1, image2, context) {
+      return infoToProcess.titlePosY;
+    };
+    
+    var watermarkPosX = function(image1, image2) {
+      return infoToProcess.watermarkPosX;
+    };
+    
+    var watermarkPosY = function(image1, image2) {
+      return infoToProcess.watermarkPosY;
+    };
+
+
     watermark([infoToProcess.coverImage, infoToProcess.watermarkImage], options)
-        .image(watermark.image.center(0.5))
+        .image(watermark.image.atPos(
+            watermarkPosX,
+            watermarkPosY,
+            infoToProcess.watermarkOpacity
+        ))
         .render()
-        .image(watermark.text.lowerLeft(
+        .image(watermark.text.atPos(
+            authorPosX,
+            authorPosY,
             infoToProcess.author,
-            '16px Josefin Slab',
+            '16px Aladin, cursive',
             infoToProcess.authorColor,
             1
         ))
         .render()
-        .image(watermark.text.lowerRight(
+        .image(watermark.text.atPos(
+            titlePosX,
+            titlePosY,
             infoToProcess.title,
-            '30px Josefin Slab',
+            '30px Aladin, cursive',
             infoToProcess.titleColor,
             1
-        ))     
+        ))  
         .then(function (img) {
             img.id = "new-image";
             downloadImage.style.display = "block";
 
+            document.getElementById('result-image').innerHTML = '';
             document.getElementById('result-image').appendChild(img);
             downloadImage.href = document.getElementById('new-image').src;
         });
